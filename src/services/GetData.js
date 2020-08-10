e7ab0566becc1693a52544644d8220b2777acb37
dudusae <dudusae@gmail.com>
2020-08-10 13:17:01 +0900

    backup

--- src/services/GetData.js
+++ src/services/GetData.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export function GetSearch(keyword) {
-    console.log('http://52.79.172.38/areas/' + keyword);
-    return axios.get('http://52.79.172.38/areas/'+ keyword);}
+    console.log('http://13.209.75.199/areas/' + keyword);
+    return axios.get('http://13.209.75.199/areas/'+ keyword);}
 
 export function GetSuggestList(keyword) {
-    console.log('http://52.79.172.38/areas');
-    return axios.get('http://52.79.172.38/areas');}        
+    console.log('http://13.209.75.199/areas');
+    return axios.get('http://13.209.75.199/areas');}        
